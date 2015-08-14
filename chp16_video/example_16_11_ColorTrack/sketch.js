68d0d5982b5e6709e8e79756bd52e7d9fd576094
Daniel Shiffman <daniel@shiffman.net>
2015-08-13 22:07:00 -0400

    a bunch of chapter 16 examples porting in progress

---
+++ chp16_video/example_16_11_ColorTrack/sketch.js
@@ -0,0 +1,83 @@
+// Learning Processing
+// Daniel Shiffman
+// http://www.learningprocessing.com
+
+// Example 16-11: Simple color tracking
+
+var video;
+
+// A variable for the color we are searching for.
+var trackColor; 
+
+function setup() {
+  createCanvas(320, 240);
+  devicePixelScaling(false);
+  video = createCapture(VIDEO);
+  video.size(width,height);
+  // The above function actually makes a separate video
+  // element on the page.  The line below hides it since we are
+  // drawing the video to the canvas
+  video.hide();
+
+  // Start off tracking for red
+  trackColor = [255, 0, 0];
+}
+
+function draw() {
+  
+
+  // Draw the video
+  image(video,0,0);
+
+  // We are going to look at the video's pixels
+  video.loadPixels();
+
+  // Before we begin searching, the "world record" for closest color is set to a high number that is easy for the first pixel to beat.
+  var worldRecord = 500; 
+
+  // XY coordinate of closest color
+  var closestX = 0;
+  var closestY = 0;
+
+  for (var y = 0; y < video.height; y++ ) {
+    for (var x = 0; x < video.width; x++ ) {
+      var loc = (x + y * video.width) * 4;
+      // The functions red(), green(), and blue() pull out the three color components from a pixel.
+      var r1 = video.pixels[loc   ]; 
+      var g1 = video.pixels[loc + 1];
+      var b1 = video.pixels[loc + 2];
+
+      var r2 = trackColor[0];
+      var g2 = trackColor[1];
+      var b2 = trackColor[2];
+
+      // Using euclidean distance to compare colors
+      var d = dist(r1, g1, b1, r2, g2, b2); // We are using the dist( ) function to compare the current color with the color we are tracking.
+
+      // If current color is more similar to tracked color than
+      // closest color, save current location and current difference
+      if (d < worldRecord) {
+        worldRecord = d;
+        closestX = x;
+        closestY = y;
+      }
+    }
+  }
+
+  // We only consider the color found if its color distance is less than 10. 
+  // This threshold of 10 is arbitrary and you can adjust this number depending on how accurate you require the tracking to be.
+  if (worldRecord < 50) { 
+    // Draw a circle at the tracked pixel
+    fill(trackColor);
+    strokeWeight(4.0);
+    stroke(0);
+    ellipse(closestX, closestY, 16, 16);
+  }
+}
+
+
+function mousePressed() {
+  // Save color where the mouse is clicked in trackColor variable
+  trackColor = video.get(mouseX,mouseY);
+  console.log(trackColor);
+}
---
+++ chp16_video/example_16_12_BackgroundRemove/sketch.js
@@ -0,0 +1,91 @@
+// Learning Processing
+// Daniel Shiffman
+// http://www.learningprocessing.com
+
+// Example 16-12: Simple background removal
+
+var video;
+
+// Saved background
+var backgroundImage;
+
+// How different must a pixel be to be a foreground pixel
+var threshold = 20;
+
+function setup() {
+  createCanvas(320, 240);
+  devicePixelScaling(false);
+  video = createCapture(VIDEO);
+  video.size(width,height);
+  video.hide();
+
+  // Create an empty image the same size as the video
+  backgroundImage = createImage(video.width, video.height, RGB);
+}
+
+function draw() {
+  
+  // Draw the video
+  image(video,0,0);
+
+  // We are going to look at everything's pixels
+  loadPixels();
+  video.loadPixels();
+  backgroundImage.loadPixels();
+
+  // Before we begin searching, the "world record" for closest color is set to a high number that is easy for the first pixel to beat.
+  var worldRecord = 500; 
+
+  // XY coordinate of closest color
+  var closestX = 0;
+  var closestY = 0;
+
+  for (var y = 0; y < video.height; y++ ) {
+    for (var x = 0; x < video.width; x++ ) {
+      var loc = (x + y * video.width) * 4;
+      
+      // Step 2, what is the foreground color
+      var r1 = video.pixels[loc   ]; 
+      var g1 = video.pixels[loc + 1];
+      var b1 = video.pixels[loc + 2];
+
+      // Step 3, what is the background color
+      var r2 = backgroundImage.pixels[loc   ]; 
+      var g2 = backgroundImage.pixels[loc + 1];
+      var b2 = backgroundImage.pixels[loc + 2];
+
+      // Step 4, compare the foreground and background color
+      var diff = dist(r1, g1, b1, r2, g2, b2);
+
+      // Step 5, Is the foreground color different from the background color
+      if (diff > threshold) {
+        // If so, display the foreground color
+        pixels[loc] = r1;
+        pixels[loc+1] = g1;
+        pixels[loc+2] = b1;
+        pixels[loc+3] = 255;
+      } else {
+        // If not, display green
+        pixels[loc] = 0;
+        pixels[loc+1] = 255;
+        pixels[loc+2] = 0;
+        pixels[loc+3] = 255;      
+      }
+    }
+  }
+}
+
+
+function mousePressed() {
+  // Copying the current frame of video into the backgroundImage object
+  // Note copy takes 5 arguments:
+  // The source image
+  // x, y, width, and height of region to be copied from the source
+  // x, y, width, and height of copy destination
+  video.loadPixels();
+  backgroundImage.loadPixels();
+  backgroundImage.copy(video, 0, 0, video.width, video.height, 0, 0, video.width, video.height);
+  backgroundImage.updatePixels();
+  console.log(backgroundImage.pixels[10000]);
+}
+
