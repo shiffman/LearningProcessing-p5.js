8278ba8b61d69f3c4acd2f0dc6110584e3b15386
Daniel Shiffman <daniel@shiffman.net>
2015-08-18 20:03:49 -0400

    cleaning port chapter 2 and 3

--- chp03_flow/example_03_02_mouseX_mouseY/sketch.js
+++ chp03_flow/example_03_02_mouseX_mouseY/sketch.js
@@ -19,5 +19,5 @@ function draw() {
   
   // mouseX is a keyword that the sketch replaces with the horizontal position of the mouse.
   // mouseY is a keyword that the sketch replaces with the vertical position of the mouse.
-  rect(mouseX,mouseY,50,50);   
+  rect(mouseX, mouseY, 50, 50);   
 }
