009ed4c537c27c802a3d79de1db2c2c6352a74e9
Dan Weiner <daweiner16@gmail.com>
2018-03-27 10:52:56 -0400

    add object branch, update with constructor

--- chp08_objects/example_08_01_singleobject/sketch.js
+++ chp08_objects/example_08_01_singleobject/sketch.js
@@ -4,31 +4,33 @@
 
 // Example 8-1: A Car class and a Car object
 
-var car; // Declare car object as a global variable.
+let car; // Declare car object as a global variable.
 
 function setup() {
   createCanvas(480, 270);
   // Initialize Car object
 
-  car = {
-    c: color(175),
-    xpos: width/2,
-    ypos: height/2,
-    xspeed: 3,
-    display: function() { // Function.
-      // The car is just a square
-      rectMode(CENTER);
-      stroke(0);
-      fill(this.c);
-      rect(this.xpos,this.ypos,20,10);
-    },
-    move: function() { // Function.
-      this.xpos = this.xpos + this.xspeed;
-      if (this.xpos > width) {
-        this.xpos = 0;
-      }
-    }
-  };
+  car = new Car();
+
+  // car = {
+  //   c: color(175),
+  //   xpos: width/2,
+  //   ypos: height/2,
+  //   xspeed: 3,
+  //   display: function() { // Function.
+  //     // The car is just a square
+  //     rectMode(CENTER);
+  //     stroke(0);
+  //     fill(this.c);
+  //     rect(this.xpos,this.ypos,20,10);
+  //   },
+  //   move: function() { // Function.
+  //     this.xpos = this.xpos + this.xspeed;
+  //     if (this.xpos > width) {
+  //       this.xpos = 0;
+  //     }
+  //   }
+  // };
 }
 
 function draw() {
@@ -36,4 +38,28 @@ function draw() {
   // Operate Car object.
   car.move(); // Operate the car object in draw( ) by calling object methods using the dots syntax.
   car.display();
+}
+
+class Car {
+  constructor(c, x, y, xspeed) {
+    this.c = color(175);
+    this.x = width/2;
+    this.y = height/2;
+    this.xspeed = 3;
+  }
+
+  display() { 
+    // The car is just a square
+    rectMode(CENTER);
+    stroke(0);
+    fill(this.c);
+    rect(this.x, this.y, 20, 10);
+  }
+
+  move() { 
+    this.x = this.x + this.xspeed;
+    if (this.x > width) {
+      this.x = 0;
+    }
+  }
 }
