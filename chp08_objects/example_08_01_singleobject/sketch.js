38471538296a91cc93864df11bea7800f9d221e3
Dan Weiner <daweiner16@gmail.com>
2018-03-27 10:58:35 -0400

    remove old code

--- chp08_objects/example_08_01_singleobject/sketch.js
+++ chp08_objects/example_08_01_singleobject/sketch.js
@@ -9,34 +9,13 @@ let car; // Declare car object as a global variable.
 function setup() {
   createCanvas(480, 270);
   // Initialize Car object
-
   car = new Car();
-
-  // car = {
-  //   c: color(175),
-  //   xpos: width/2,
-  //   ypos: height/2,
-  //   xspeed: 3,
-  //   display: function() { // Function.
-  //     // The car is just a square
-  //     rectMode(CENTER);
-  //     stroke(0);
-  //     fill(this.c);
-  //     rect(this.xpos,this.ypos,20,10);
-  //   },
-  //   move: function() { // Function.
-  //     this.xpos = this.xpos + this.xspeed;
-  //     if (this.xpos > width) {
-  //       this.xpos = 0;
-  //     }
-  //   }
-  // };
 }
 
 function draw() {
   background(100);
   // Operate Car object.
-  car.move(); // Operate the car object in draw( ) by calling object methods using the dots syntax.
+  car.move(); // 
   car.display();
 }
 
